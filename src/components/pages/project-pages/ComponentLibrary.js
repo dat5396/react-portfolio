76e527d82943a21d0f9a4d3aef699a57e63462cf
dat5396 <104238852+dat5396@users.noreply.github.com>
2026-06-18 18:56:54 -0400

    June 18

--- src/components/pages/project-pages/ComponentLibrary.js
+++ src/components/pages/project-pages/ComponentLibrary.js
@@ -9,11 +9,11 @@ function ComponentLibrary() {
 
     const sections = [
         { id: 'section1', title: '1. Project overview' },
-        { id: 'section2', title: '2. Part A' },
-        { id: 'section3', title: '3. Part B' },
-        { id: 'section4', title: '4. Part C' },
-        { id: 'section5', title: '5. Part D' },
-        { id: 'section6', title: '6. Outcome & impact' },
+        { id: 'section2', title: '2. General' },
+        { id: 'section3', title: '3. Data entry' },
+        { id: 'section4', title: '4. Data display' },
+        { id: 'section5', title: '5. Navigation' },
+        { id: 'section6', title: '6. Feedback' },
     ];
 
     useEffect(() => {
@@ -52,80 +52,70 @@ function ComponentLibrary() {
 
     return (
         <div className="project__container audience__recommendation" >
-            <div>
+            {/* <div>
                 <img src='images/project images/AR/AR-00.png' className='large__image'></img>
-            </div>
+            </div> */}
 
             <div id="section1">
                 <div className='section__name'>
-                    <h2>1. Part A</h2>
+                    <h2>1. Gerneral</h2>
                 </div>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A1.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A2.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A3.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A4.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A5.png' className='large__image'></img>
+                <p>Lorem ipsum</p>
             </div>
 
             <div id="section2">
                 <div className='section__name'>
-                    <h2>2. Part A</h2>
+                    <h2>2. Gerneral</h2>
                 </div>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A1.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A2.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A3.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A4.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartA_A5.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-A01.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-A02.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-A03.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-A04.png' className='large__image'></img>
             </div>
 
             <div id="section3">
                 <div className='section__name'>
-                    <h2>3. Part B</h2>
+                    <h2>3. Data Entry</h2>
                 </div>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B1.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B2.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B3.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B4.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B5.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B6.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B7.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartB_B8.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-B01.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-B02.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-B03.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-B04.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-B05.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-B06.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-B07.png' className='large__image'></img>
             </div>
 
             <div id="section4">
                 <div className='section__name'>
-                    <h2>4. Part C</h2>
+                    <h2>4. Data Display</h2>
                 </div>
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C1.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C2.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C3.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C4.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C5.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C6.png' className='large__image'></img>
-                {/* <img loading="lazy" src='images/project images/DS/DS-PartC_C7.png' className='large__image'></img> */}
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C8.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartC_C9.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-C01.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-C02.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-C03.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-C04.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-C05.png' className='large__image'></img>
             </div>
 
             <div id="section5">
                 <div className='section__name'>
-                    <h2>5. Part D</h2>
+                    <h2>5. Navigation</h2>
                 </div>
-                <img loading="lazy" src='images/project images/DS/DS-PartD_D1.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartD_D2.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartD_D3.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartD_D4.png' className='large__image'></img>
-                <img loading="lazy" src='images/project images/DS/DS-PartD_D5.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-D01.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-D02.png' className='large__image'></img>
             </div>
 
-            <div id="section5">
+            <div id="section6">
                 <div className='section__name'>
-                    <h2>6. OUTCOME & IMPACT</h2>
+                    <h2>6. Feedback</h2>
                 </div>
-                <p className='space'>A</p>
-                <p>As a result, advertisers were able to identify the right audiences more quickly and with greater confidence. The AI recommendations reduced the time spent searching through third-party segments and made the targeting process more efficient. Campaign performance improved through better audience selection, leading to higher impressions and stronger overall results. This solution helped advertisers make smarter decisions and optimize their campaigns more effectively.</p>
-                <img loading="lazy" src='images/illumin-logo.svg' className='logo'></img>
+                <img loading="lazy" src='images/project images/DS/DS-E01.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-E02.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-E03.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-E04.png' className='large__image'></img>
+                <img loading="lazy" src='images/project images/DS/DS-E05.png' className='large__image'></img>
             </div>
+
             <div>
                 <Sidebar activeSection={activeSection} sections={sections} handleLinkClick={handleLinkClick} />
             </div>
